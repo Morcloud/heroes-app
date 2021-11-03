4df76aaf4b3bd707c876d7ace307832c085be671
Ricardo Tornero <richardo@outlook.com>
2021-11-02 22:22:43 -0600

    tarjetas de información de heroe

--- src/components/dc/DcScreen.jsx
+++ src/components/dc/DcScreen.jsx
@@ -3,8 +3,10 @@ import { HeroList } from "../heroes/HeroList";
 
 export const DcScreen = () => {
     return (
-        <>
+        <div>
+            <h1>DC Comics</h1>
+            <hr />
             <HeroList publisher='DC Comics' />
-        </>
+        </div>
     );
 };
--- src/components/heroes/HeroList.jsx
+++ src/components/heroes/HeroList.jsx
@@ -1,12 +1,13 @@
 import { getHeroesByPublisher } from "../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <ul className='card-columns'>
             {heroes.map((hero) => (
-                <li key={hero.id}>{hero.superhero}</li>
+                <HeroCard key={hero.id} {...hero} />
             ))}
         </ul>
     );
