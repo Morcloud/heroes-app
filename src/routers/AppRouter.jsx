49750bc116919b87e18b8bda8609950986149cfb
Ricardo Tornero <richardo@outlook.com>
2021-11-02 21:03:02 -0600

    Creación segundo router

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { LoginSreen } from "../components/login/LoginSreen";
-import { Navbar } from "../components/ui/Navbar";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
-                <Navbar />
                 <Switch>
                     <Route exact path='/login' component={LoginSreen} />
-                    <Route exact path='/' component={MarvelScreen} />
+                    <Route path='/' component={DashboardRoutes} />
                 </Switch>
             </div>
         </Router>
---
+++ src/routers/DashboardRoutes.jsx
@@ -0,0 +1,26 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { Navbar } from "../components/ui/Navbar";
+import { HeroScreen } from "../components/heroes/HeroScreen";
+import { DcScreen } from "../components/dc/DcScreen";
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+            <Navbar />
+            <div>
+                <Switch>
+                    <Route exact path='/marvel' component={MarvelScreen} />
+                    <Route exact path='/dc' component={DcScreen} />
+                    <Route
+                        exact
+                        path='/heroe/:heroeid'
+                        component={HeroScreen}
+                    />
+
+                    <Redirect to='/marvel' />
+                </Switch>
+            </div>
+        </>
+    );
+};
